9d473c8dcac4a1c21dc49ec3eb84c2da8a08cf43
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-08 11:23:58 +0700

    initial implementation (#1)
    * initial implementation
    
    * remove requirements.yml
    
    * rubocopfy

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,54 +1,59 @@
 require "spec_helper"
 require "serverspec"
 
-package = "nfs-client"
-service = "nfs-client"
-config  = "/etc/nfs-client/nfs-client.conf"
-user    = "nfs-client"
-group   = "nfs-client"
-ports   = [PORTS]
-log_dir = "/var/log/nfs-client"
-db_dir  = "/var/lib/nfs-client"
+ports = []
+default_user = "root"
+default_group = "root"
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/nfs-client.conf"
-  db_dir = "/var/db/nfs-client"
-end
-
-describe package(package) do
-  it { should be_installed }
-end
-
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("nfs-client") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  default_group = "wheel"
 end
 
 case os[:family]
 when "freebsd"
-  describe file("/etc/rc.conf.d/nfs-client") do
+  describe file("/etc/rc.conf.d/lockd") do
+    it { should exist }
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    its(:content) { should match(/^rpc_lockd_enable="YES"$/) }
+    its(:content) { should match(/^rpc_lockd_flags="-h 10\.0\.2\.15"$/) }
+  end
+
+  describe service("lockd") do
+    it { should be_running }
+  end
+
+  describe file("/etc/rc.conf.d/statd") do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    its(:content) { should match(/^rpc_statd_enable="YES"$/) }
+    its(:content) { should match(/^rpc_statd_flags="-h 10\.0\.2\.15"$/) }
+  end
+
+  describe service("statd") do
+    it { should be_running }
+  end
+
+  describe command("service rpcbind onestatus") do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/^rpcbind is running as pid \d+\.$/) }
+    its(:stderr) { should eq "" }
   end
 end
 
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
+describe file("/etc/fstab") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  its(:content) { should match(%r{^#{Regexp.escape("127.0.0.1:/exports/foo")}\s+\/mnt\s+nfs\s+ro\s+0\s+0}) }
 end
 
 ports.each do |p|
